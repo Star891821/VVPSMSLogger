876728e73bb46b535d571aa5d074c07d4164b25d
Star891821 <star891821@gmail.com>
2023-11-21 21:24:34 +0530

    Below changes done: ID : 1. Fix column width to 6-7 characters. 2. Created on : Change date format to dd/mm/yyyy <br> hh:mm:ss 3. Created on : Fix width to 10 characters. export to excel : Rename file to <logfile_datetime(yyyymmddhhmmss)> 4. Message/Stack trace/URL : We should be seeing more information than this in the view to help debug. 5. URL : Remove baseURL (as in webconfig)

--- VVPSMSLogger/wwwroot/js/logTable.js
+++ VVPSMSLogger/wwwroot/js/logTable.js
@@ -1,13 +1,12 @@
 ﻿var dataTable
 $(document).ready(function () {
 
-    debugger
+    //debugger
     var fromDate;
     var toDate;
     $("#fromDate").datepicker({
         onSelect: function (dateText) {
             console.log("Selected date: " + dateText + "; input's current value: " + this.value);
-            debugger;
             fromDate = dateText;
             bindDataTable();
             dataTable.draw();
@@ -33,15 +32,74 @@ $(document).ready(function () {
         dataTable.draw();
     });
 
-    var jsonData = []
-    jsonData.push({
-        "contact_name": 'hello',
-        "company_name": 'hi'
-    })
+   
     bindDataTable();
 
+    // Show more/less functionality
+    $('#logTable').on('click', '.more-link', function (e) {
+        e.preventDefault();
+        var $moreLink = $(this);
+        var $moreContent = $moreLink.prev('.more-content');
+        var $ellipsis = $moreLink.prev('.more');
+
+        if ($moreLink.text() === '...More') {
+            $moreLink.text(' Less');
+            $moreContent.show();
+            $ellipsis.hide();
+        } else {
+            $moreLink.text('...More');
+            $moreContent.hide();
+            $ellipsis.show();
+        }
+    });
+
+    function getFormattedDateTime() {
+        const padL = (nr, len = 2, chr = `0`) => `${nr}`.padStart(len, chr);
+        const date = new Date();
+        const year = date.getFullYear();
+        const month = padL(date.getMonth() + 1);
+        const day = padL(date.getDate());
+        const hours = padL(date.getHours());
+        const minutes = padL(date.getMinutes());
+        const seconds = padL(date.getSeconds());
+
+        return `logfile_${year}${month}${day}${hours}${minutes}${seconds}`;
+    }
+
+    function createEllipsisRenderConfig(target, maxLength = 50) {
+        return {
+            targets: target,
+            render: $.fn.dataTable.render.ellipsis(maxLength)
+        };
+    }
+
+    function createDateTimeRenderConfig(target) {
+        return {
+            targets: target,
+            render: function (data, type, full, meta) {
+                var date = new Date(data);
+                const padL = (nr, len = 2, chr = `0`) => `${nr}`.padStart(len, chr);
+                return `${padL(date.getDate())}/${padL(date.getMonth() + 1)}/${date.getFullYear()} ${padL(date.getHours())}:${padL(date.getMinutes())}:${padL(date.getSeconds())}`;
+            }
+        };
+    }
+    function createShowMoreRenderConfig(target, showChar = 100) {
+        return {
+            targets: target,
+            render: function (data, type, row) {
+                if (data.length > showChar) {
+                    var content = data.substr(0, showChar);
+                    var hiddenContent = '<span class="more-content" style="display:none;">' + data.substr(showChar, data.length - showChar) + '</span>';
+                    var moreLink = '<a href="#" class="more-link">...More</a>';
+                    return '<span class="more">' + content + '</span>' + hiddenContent + moreLink;
+                } else {
+                    return data;
+                }
+            }
+        };
+    }
+
     function bindDataTable() {
-        debugger
         dataTable = $("#logTable").DataTable({
             "processing": true,
             "serverSide": true,
@@ -50,12 +108,11 @@ $(document).ready(function () {
             bRetrieve: true,
             crossDomain: true,
             "ajax": {
-                "url": "https://localhost:7188/api/Logger/LoadData",
+                "url": "https://projects.sustainedgeconsulting.com/VVPSMS/V0/VVPSMSAPI/api/Logger/LoadData",
                 "type": "GET",
                 "datatype": "json",
                 //"data": { name: fromDate },
                 "data": function (d) {
-                    debugger
                     d.fromDate = $('#fromDate').val();
                     d.toDate = $('#toDate').val();
                     d.level = $('#level').val();
@@ -67,7 +124,7 @@ $(document).ready(function () {
             buttons: [
                 {
                     extend: 'excelHtml5',
-                    title: 'LogExcel',
+                    title: getFormattedDateTime(),
                     text: 'Export to excel',
                     exportOptions: { orthogonal: 'export' }
                     //Columns to export
@@ -91,62 +148,43 @@ $(document).ready(function () {
             ],
             order: [[1, 'desc']],
             columnDefs: [
+                createEllipsisRenderConfig(0, 50),
+                createDateTimeRenderConfig(1),
+                createShowMoreRenderConfig(3, 50), 
+                createShowMoreRenderConfig(4, 100), 
+                createShowMoreRenderConfig(5, 100), 
+                createEllipsisRenderConfig(6, 50),
                 {
-                    render: $.fn.dataTable.render.ellipsis(50),
-                    targets: 0
-                },
-                {
-                    render: function (data, type, full, meta) {
-                        var date = new Date(data);
-
-                        const padL = (nr, len = 2, chr = `0`) => `${nr}`.padStart(2, chr);
-
-                        return `${padL(date.getMonth() + 1)}/${padL(date.getDate())}/${date.getFullYear()} ${padL(date.getHours())}:${padL(date.getMinutes())}:${padL(date.getSeconds())}`
-                       
-                    },
-                    targets: 1
-                },
-                {
-                    render: $.fn.dataTable.render.ellipsis(50),
-                    targets: 3
-                },
-                {
-                    render: $.fn.dataTable.render.ellipsis(50),
-                    targets: 4
-                },
-                {
-                    render: $.fn.dataTable.render.ellipsis(50),
-                    targets: 5
-                },
-                {
-                    render: $.fn.dataTable.render.ellipsis(50),
-                    targets: 6
-                },
-                {
-                    render: $.fn.dataTable.render.ellipsis(50),
-                    targets: 7
+                    targets: 7,
+                    render: function (data) {
+                        // Find the index of "/api/" in the URL
+                        const apiUrlIndex = data.indexOf('/api/');
+                        if (apiUrlIndex !== -1) {
+                            data = data.substring(apiUrlIndex + 5); // Get the part of the URL after "/api/"
+                        }
+                       // return data; // Return original data if "/api/" is not found
+                        var showChar = 10;
+                        if (data.length > showChar) {
+                            var content = data.substr(0, showChar);
+                            var hiddenContent = '<span class="more-content" style="display:none;">' + data.substr(showChar, data.length - showChar) + '</span>';
+                            var moreLink = '<a href="#" class="more-link">...More</a>';
+                            return '<span class="more">' + content + '</span>' + hiddenContent + moreLink;
+                        } else {
+                            return data;
+                        }
+                    }
                 }
-                //{
-                //    "render": function (data, type, row) {
-                //        debugger
-                //        if (data == null) { return "" }
-                //        else {
-                //            return data;
-                //        }
-                //    },
-                //    targets: 8
-                //}
             ],
             fixedColumns: true,
             "columns": [
-                { "data": "id", "name": "Id", width: '30px', style: 'white-space: normal' },
-                { "data": "createdOn", "name": "Created On", width: '80px', style: 'white-space: normal' },
-                { "data": "level", "name": "Level", width: '30px', style: 'white-space: normal' },
-                { "data": "message", "name": "Message", width: '80px', class: 'text-wrap' },
-                { "data": "stackTrace", "name": "Stack Trace", width: '80px', class: 'text-wrap', style: 'word-break: break-word;' },
-                { "data": "exception", "name": "Exception", width: '80px', class: 'text-wrap', style: 'word-break: break-word;' },
-                { "data": "logger", "name": "Logger", width: '60px', class: 'text-wrap', style: 'word-break: break-word;' },
-                { "data": "url", "name": "Url", width: '80px', class: 'text-wrap', style: 'word-break: break-word;' }
+                { "data": "id", "name": "Id", width: '5px', class: 'text-wrap', style: 'word-break: break-word;' },
+                { "data": "createdOn", "name": "Created On", width: '15px', class: 'text-wrap', style: 'word-break: break-word;' },
+                { "data": "level", "name": "Level", width: '15px', class: 'text-wrap', style: 'word-break: break-word;' },
+                { "data": "message", "name": "Message", width: '90px', class: 'text-wrap' },
+                { "data": "stackTrace", "name": "Stack Trace", width: '90px', class: 'text-wrap', style: 'word-break: break-word;' },
+                { "data": "exception", "name": "Exception", width: '90px', class: 'text-wrap', style: 'word-break: break-word;' },
+                { "data": "logger", "name": "Logger", width: '40px', class: 'text-wrap', style: 'word-break: break-word;' },
+                { "data": "url", "name": "Url", width: '50px', class: 'text-wrap', style: 'word-break: break-word;' }
                 //{ "data": "formId", "name": "Form Id", width: '80px', class: 'text-wrap', style: 'word-break: break-word;' }
 
             ]
